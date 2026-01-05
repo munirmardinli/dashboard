aefa043a6401019cd160a586a2b676b413e4ebd7
Munir Mardinli <munir.mardinli@icloud.com>
2026-01-05 23:22:39 +0100

    chore: update environment configuration for production and development
    - Removed unnecessary environment variables from the production CI configuration.
    - Updated the example environment file to reflect the removal of the base URL variable.
    - Adjusted the robots generation logic to use a static sitemap path and host.

--- apps/portfolio/src/app/robots.ts
+++ apps/portfolio/src/app/robots.ts
@@ -4,18 +4,14 @@ export const dynamic = 'force-static';
 
 class RobotsManager {
 	static generateRobots(): MetadataRoute.Robots {
-		const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-		if (!baseUrl) {
-			throw new Error('NEXT_PUBLIC_BASE_URL is not set');
-		}
 		return {
 			rules: {
 				userAgent: '*',
 				allow: ['/'],
 				disallow: ['/admin', '/privacy', '/user/*'],
 			},
-			sitemap: `${baseUrl}/sitemap.xml`,
-			host: baseUrl,
+			sitemap: `/portfolio/sitemap.xml`,
+			host: '/',
 		};
 	}
 }
